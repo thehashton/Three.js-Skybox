00ca60cf7ed2c0cf9a7f9060508004f3a4a8fc87
admin <admin@admins-MacBook-Pro.local>
2017-12-23 14:43:54 +0000

    adds a spiral 3d mesh sphere enviorenment around the camera

--- js/script.js
+++ js/script.js
@@ -5,9 +5,27 @@ var renderer = new THREE.WebGLRenderer( );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
+//create the shape
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+
+// create a material, colour or image texture
+// var material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: false} );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe: true} );
+var sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+// Camera Position
+camera.position.z = 3;
+
 //game logic
 var update = function ( ) {
-
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.005;
 };
 
 //render logic
