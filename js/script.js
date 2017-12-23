81eb7a4bf987d4f2afd97b1fd0588ab0a9da4287
admin <admin@admins-MacBook-Pro.local>
2017-12-23 22:29:26 +0000

    adds cube geometry for skybox

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // FPS
-(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()
+(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})();
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -19,10 +19,9 @@ window.addEventListener( 'resize', function() {
 
 // controls
 controls = new THREE.OrbitControls( camera, renderer.domElement);
-//controls = new THREE.FlyControls(camera, renderer.domElement);
 
 // Object loader
-var loader = new THREE.ObjectLoader();
+// var loader = new THREE.ObjectLoader();
 
 // loader.load (
 //     ' objects/mars-rover.json ',
@@ -32,57 +31,28 @@ var loader = new THREE.ObjectLoader();
 // );
 
 // creates the shape
-var geometry = new THREE.BoxGeometry( 2, 2, 2 );
+var geometry = new THREE.CubeGeometry( 250, 250, 250 );
 var cubeMaterials = [
-    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_rt.png' ), side: THREE.DoubleSide }), //right side
-    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_lf.png' ), side: THREE.DoubleSide }), //left side
-    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_dn.png' ), side: THREE.DoubleSide }), //bottom side
-    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_up.png' ), side: THREE.DoubleSide }), //top side
-    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_ft.png' ), side: THREE.DoubleSide }), //front side
-    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_bk.png' ), side: THREE.DoubleSide }) //back side
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "img/nightsky_ft.png" ), side: THREE.DoubleSide }), //front side
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_bk.png' ), side: THREE.DoubleSide }), //back side
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_up.png' ), side: THREE.DoubleSide }), //up side
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_dn.png' ), side: THREE.DoubleSide }), //down side
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_rt.png' ), side: THREE.DoubleSide }), //right side
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/nightsky_lf.png' ), side: THREE.DoubleSide }) //left side
 ];
 
-
-//Floor
-var floorGeometry = new THREE.CubeGeometry( 10, 1, 10);
-var floorMaterial = new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/nightsky_rt.png'), side: THREE.DoubleSide });
-var floorCube = new THREE.Mesh( floorGeometry, floorMaterial);
-floorCube.position.y = -5;
-scene.add( floorCube );
-
-//Ceiling
-var ceilingGeometry = new THREE.CubeGeometry( 10, 1, 10);
-var ceilingMaterial = new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/nightsky_rt.png'), side: THREE.DoubleSide });
-var ceilingCube = new THREE.Mesh( ceilingGeometry, ceilingMaterial);
-ceilingCube.position.y = 5;
-scene.add( ceilingCube );
-
-//Left Wall
-var leftWallGeometry = new THREE.CubeGeometry( 1, 10, 10);
-var leftWallMaterial = new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/nightsky_rt.png'), side: THREE.DoubleSide });
-var leftWallCube = new THREE.Mesh( leftWallGeometry, leftWallMaterial);
-leftWallCube.position.x = -5;
-scene.add( leftWallCube );
-
-//Right Wall
-var rightWallGeometry = new THREE.CubeGeometry( 1, 10, 10);
-var rightWallMaterial = new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/nightsky_rt.png'), side: THREE.DoubleSide });
-var rightWallCube = new THREE.Mesh( rightWallGeometry, rightWallMaterial);
-rightWallCube.position.x = 5;
-scene.add( rightWallCube );
-
-// create a material, colour or image texture
-// var material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: true} );
-var material = new THREE.MeshFaceMaterial( cubeMaterials );
-var cube = new THREE.Mesh( geometry, material );
+var cubeMaterial = new THREE.MeshFaceMaterial( cubeMaterials );
+var cube = new THREE.Mesh( geometry, cubeMaterial );
 scene.add( cube );
+
 
 // Camera Position
 camera.position.z = 3;
 
 // lighting
-var ambientLight = new THREE.AmbientLight( 0xFFFFFF, 3 );
+var ambientLight = new THREE.AmbientLight( 0xFFFFFF, 0.3 );
 scene.add( ambientLight );
+
 
 
 //game logic
